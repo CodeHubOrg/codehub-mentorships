f0ff40558f945c60d0118e04cd51cf13ee521639
r1903 <55586316+r1903@users.noreply.github.com>
2020-03-31 21:40:30 +0100

    added head component and AuthLayout

--- resources/js/Atoms/Heading.tsx
+++ resources/js/Atoms/Heading.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 
 interface Props {
-    content: any
+    children: React.ReactChildren;
+    type:"h1" | "h2" | "h3";
 }
 
-const Heading: React.FC<Props> = ({content}) =>  {
+
+export default function Heading({children,type}: Props) {
+
     return (
         <div className="box-border p-4 border-4 border-gray-400 bg-gray-200">
-            <h1 className="font-mono text-lg text-gray-800 text-center">{content}</h1>
+            {type === "h1" && <h1 className="font-mono text-4xl text-gray-800 text-center">{children}</h1> }
+            {type === "h2" && <h2 className="font-mono text-3xl text-gray-800 text-center">{children}</h2> }
+            {type === "h3" && <h3 className="font-mono text-2xl text-gray-800 text-center">{children}</h3> }
         </div>
     );
 }
 
 
-export default Heading;
