81ba86b33f07605d7ee4261729b8df48626928ce
Katja Durrani <mail@kdurrani..co.uk>
2020-04-03 03:51:06 +0100

    add AuthLayout to index

--- resources/js/Atoms/Button.tsx
+++ resources/js/Atoms/Button.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-
+import React from "react";
 
 interface Props {
-    children: React.ReactChildren
-    size: "small" | "medium" | "large"
+    children: React.ReactChildren;
+    size: "small" | "medium" | "large";
 }
 
-export default function Button({children, size}: Props) {
+export default function Button({ children, size }: Props) {
+    const handleClick = () => {
+        console.log("I was clicked");
+    };
 
     let styling;
     switch (size) {
         case "small":
-            styling = "inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-small rounded-md text-white bg-red-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150";
-        break;
+            styling =
+                "inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-small rounded-md text-white bg-red-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150";
+            break;
         case "medium":
-            styling = "inline-flex items-center px-8 py-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-blue-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150";
-        break;
+            styling =
+                "inline-flex items-center px-8 py-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-blue-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150";
+            break;
         default:
             styling = "";
     }
 
     return (
-        <button type="button" className={styling}>{children}</button>
+        <button type="button" onClick={handleClick} className={styling}>
+            {children}
+        </button>
     );
 }
