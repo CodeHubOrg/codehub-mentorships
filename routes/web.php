0ad9512d013570052852ed9fa1404d946a567dea
Alison Yoon <alison@192.168.1.102>
2020-04-19 18:15:38 +0100

    alison/authentication laravel auth installed. webpack.mix.js modified

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,7 @@ Route::get('/', function () {
 });
 
 Route::get('/admin', [AdminDashboardController::class, 'index']);
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,27 +1,7 @@
-const mix = require('laravel-mix');
-const path = require('path');
-
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
-mix.ts('resources/js/app.tsx', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        require('tailwindcss'),
-        require('postcss-import')
-    ])
-    .webpackConfig({
-        output: {
-            chunkFilename: 'js/[name].js?id=[chunkhash]',
-        },
-        resolve: {
-            alias: {'@': path.resolve('resources/js')},
-        }
-    });
+const mix = require("laravel-mix"); const path = require("path");
+/* |--------------------------------------------------------------------------
+| Mix Asset Management |-------------------------------------------------------------------------- |
+ | Mix provides a clean, fluent API for defining some Webpack build steps |
+ for your Laravel application. By default, we are compiling the Sass |
+ file for the application as well as bundling up all the JS files. | */
+mix.ts("resources/js/app.tsx", "public/js") .postCss("resources/css/app.css", "public/css", [ require("tailwindcss"), require("postcss-import"), ]) .webpackConfig({ output: { chunkFilename: "js/[name].js?id=[chunkhash]", }, resolve: { alias: { "@": path.resolve("resources/js") }, }, }); mix.react("resources/js/app.js", "public/js").sass( "resources/sass/app.scss", "public/css" );
