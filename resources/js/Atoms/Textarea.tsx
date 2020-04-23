b8b644ec087e00269839c5e172dcd6d79c00f456
r1903 <55586316+r1903@users.noreply.github.com>
2020-04-23 17:51:47 +0100

    added the more props the component

--- resources/js/Atoms/Textarea.tsx
+++ resources/js/Atoms/Textarea.tsx
@@ -1,25 +1,32 @@
 import * as React from "react";
 
-
 interface iFieldProps {
-    placeholder?: string;
+	name: string;
+	label?: string;
+	value?: string;
+	helpText?: string;
+	onChange: (e: React.FormEvent<HTMLTextAreaElement>) => void;
 }
 
 export const Textarea: React.FC<iFieldProps> = ({
-    placeholder
+    name,
+	label,
+	value,
+	onChange,
+	helpText
 }) => {
     return (
         <div>
-            <label className="block text-sm font-medium text-gray-700">
-                Textarea
+            <label htmlFor={name} className="block text-sm font-medium text-gray-700">
+                {label}
             </label>
              <textarea
-             placeholder={placeholder}
-             onChange={
-                 (e: React.FormEvent<HTMLTextAreaElement>) =>
-                     console.log(e.currentTarget.value)
-             }
-             className="form-input block w-full pl-7 pr-12 sm:text-sm sm:leading-5"
+                id={name}
+                name={name}
+                value={value}
+                className="form-input block w-full pl-7 pr-12 sm:text-sm sm:leading-5"
+                onChange={onChange}
+                placeholder={helpText}
              />
         </div>
     );
