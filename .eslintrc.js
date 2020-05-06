b795270ac684da4acdc2ad9f97b1b36a9e5bc121
Thea Lanherne <thea.lanherne@ghyston.com>
2020-05-06 18:59:29 +0100

    Add prettier and make it run as part of eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,53 +1,33 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+    env: {
+        browser: true,
+        es6: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/eslint-recommended"
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:prettier/recommended',
     ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
     },
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
         },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
+        ecmaVersion: 2018,
+        sourceType: 'module',
     },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "react/prop-types": [
-            0
-        ]
+    plugins: ['react', '@typescript-eslint'],
+    rules: {
+        'react/prop-types': [0],
     },
-    "settings": {
-        "react": {
-            "version": "detect"
-        }
-    }
-}
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+};
