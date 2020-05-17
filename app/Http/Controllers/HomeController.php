fba166bcb7e0ad8d98b80abcf065e2693bc3d310
John Wyles <john@fourstacks.co.uk>
2020-05-17 22:24:56 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class HomeController extends Controller
@@ -25,7 +24,7 @@ class HomeController extends Controller
     public function index()
     {
         //return view('home');
-        return Inertia::render('Home', 
+        return Inertia::render('Home',
             ['heading' => 'This will be the user account page.']
         );
     }
