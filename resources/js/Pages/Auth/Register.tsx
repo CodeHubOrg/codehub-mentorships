1c3d7dbe2395b54a533ad2cb811078628e901bc2
Thea Lanherne <thea.lanherne@ghyston.com>
2020-05-26 19:29:39 +0100

    Fix lint

--- resources/js/Pages/Auth/Register.tsx
+++ resources/js/Pages/Auth/Register.tsx
@@ -4,106 +4,106 @@ import FormTextInput from "@/Molecules/FormTextInput";
 import AuthLayout from "@/Atoms/AuthLayout";
 
 type RegisterFormValues = {
-	email: string;
-	password: string;
-	confirmpwd: string;
-	fullname: string;
+    email: string;
+    password: string;
+    confirmpwd: string;
+    fullname: string;
 };
 
 const validate = (values: RegisterFormValues) => {
-	let errors: IErrors<RegisterFormValues> = {};
+    let errors: IErrors<RegisterFormValues> = {};
 
-	if (!values.fullname || values.fullname.trim().length === 0) {
-		errors.fullname = "Please enter your fullname";
-	}
+    if (!values.fullname || values.fullname.trim().length === 0) {
+        errors.fullname = "Please enter your fullname";
+    }
 
-	let validEmail = /^.+@.+\..+$/;
-	if (!validEmail.test(values.email)) {
-		errors.email = "Please enter valid email address";
-	}
+    let validEmail = /^.+@.+\..+$/;
+    if (!validEmail.test(values.email)) {
+        errors.email = "Please enter valid email address";
+    }
 
-	if (values.password.trim().length < 8) {
-		errors.password = "password length should be at least 8 characters.";
-	}
-	if (values.password !== values.confirmpwd) {
-		errors.confirmpwd = "Passwords do not match.";
-	}
+    if (values.password.trim().length < 8) {
+        errors.password = "password length should be at least 8 characters.";
+    }
+    if (values.password !== values.confirmpwd) {
+        errors.confirmpwd = "Passwords do not match.";
+    }
 
-	return errors;
+    return errors;
 };
 
 const RegisterForm: React.FC = () => {
-	return (
-		<AuthLayout
-			heading="Register"
-			message="Please register to create a profile"
-		>
-			<Form<RegisterFormValues>
-				action=""
-				initialValues={{
-					fullname: "",
-					email: "",
-					password: "",
-					confirmpwd: "",
-				}}
-				validate={validate}
-				button="Register"
-				render={(values, handleChange, errors) => (
-					<React.Fragment>
-						<FormTextInput
-							type="text"
-							name="fullname"
-							label="Full Name"
-							value={values.fullname}
-							helpText=""
-							onChange={handleChange}
-						/>
-						<div className="block text-sm font-medium text-red-500 pb-5">
-							{errors.fullname}
-						</div>
-						<FormTextInput
-							type="email"
-							name="email"
-							label="Email address"
-							value={values.email}
-							helpText=""
-							onChange={handleChange}
-						/>
-						<div className="block text-sm font-medium text-red-500 pb-5">
-							{errors.email}
-						</div>
-						<FormTextInput
-							type="password"
-							name="password"
-							label="Password"
-							value={values.password}
-							helpText=""
-							onChange={handleChange}
-						/>
+    return (
+        <AuthLayout
+            heading="Register"
+            message="Please register to create a profile"
+        >
+            <Form<RegisterFormValues>
+                action=""
+                initialValues={{
+                    fullname: "",
+                    email: "",
+                    password: "",
+                    confirmpwd: "",
+                }}
+                validate={validate}
+                button="Register"
+                render={(values, handleChange, errors) => (
+                    <React.Fragment>
+                        <FormTextInput
+                            type="text"
+                            name="fullname"
+                            label="Full Name"
+                            value={values.fullname}
+                            helpText=""
+                            onChange={handleChange}
+                        />
+                        <div className="block text-sm font-medium text-red-500 pb-5">
+                            {errors.fullname}
+                        </div>
+                        <FormTextInput
+                            type="email"
+                            name="email"
+                            label="Email address"
+                            value={values.email}
+                            helpText=""
+                            onChange={handleChange}
+                        />
+                        <div className="block text-sm font-medium text-red-500 pb-5">
+                            {errors.email}
+                        </div>
+                        <FormTextInput
+                            type="password"
+                            name="password"
+                            label="Password"
+                            value={values.password}
+                            helpText=""
+                            onChange={handleChange}
+                        />
 
-						<div className="block text-sm font-medium text-red-500 pb-5">
-							{errors.password}
-						</div>
-						<FormTextInput
-							type="password"
-							name="confirmpwd"
-							label="Confirm Password"
-							value={values.confirmpwd}
-							helpText=""
-							onChange={handleChange}
-						/>
+                        <div className="block text-sm font-medium text-red-500 pb-5">
+                            {errors.password}
+                        </div>
+                        <FormTextInput
+                            type="password"
+                            name="confirmpwd"
+                            label="Confirm Password"
+                            value={values.confirmpwd}
+                            helpText=""
+                            onChange={handleChange}
+                        />
 
-						<div className="block text-sm font-medium text-red-500 pb-5">
-							{errors.confirmpwd}
-						</div>
-					</React.Fragment>
-				)}
-			/>
-			<div className="font-medium text-indigo-600 pt-5 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">
-				<a href="/login">Login</a>
-			</div>
-		</AuthLayout>
-	);
+                        <div className="block text-sm font-medium text-red-500 pb-5">
+                            {errors.confirmpwd}
+                        </div>
+                    </React.Fragment>
+                )}
+            />
+            <div className="font-medium text-indigo-600 pt-5 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">
+                <a href="/login">Login</a>
+            </div>
+        </AuthLayout>
+    );
 };
 
 export default RegisterForm;
