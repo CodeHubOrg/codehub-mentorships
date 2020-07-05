aa2eec52c2efe349df58a67f70700fb823a37d0b
r1903 <55586316+r1903@users.noreply.github.com>
2020-07-05 18:00:24 +0100

    added style to dashboard component

--- resources/js/Atoms/FormLayout.tsx
+++ resources/js/Atoms/FormLayout.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 interface Props {
     children: React.ReactNode;
-    heading: string;
-    message: string;
+    heading?: string;
+    message?: string;
 }
 
 export default function FormLayout({ children, heading, message }: Props) {
@@ -16,7 +16,7 @@ export default function FormLayout({ children, heading, message }: Props) {
             </div>
             <div className="min-h-screen bg-gray-50  flex flex-col py-12 sm:px-6 lg:px-20">
                 <div className="bg-white py-2 px-19 shadow sm:rounded-lg sm:px-20">
-                    <h2 className="mb-6 text-3xl leading-9 font-extrabold text-gray-900 p-4 text-center">
+                    <h2 className="mb-6 text-2xl leading-9 font-extrabold text-gray-900 p-4 text-center">
                         {heading}
                     </h2>
                     <p className="mb-6 text-1xl font-semibold leading-5 text-gray-800 max-w p-19">
--- resources/js/Pages/Profiles/Index.tsx
+++ resources/js/Pages/Profiles/Index.tsx
@@ -2,16 +2,73 @@ import React from "react";
 import { InertiaLink } from "@inertiajs/inertia-react";
 import FormLayout from "@/Atoms/FormLayout";
 
-interface Props {
-    heading?: string;
-}
 
-const Index = ({ heading }: Props) => {
+
+const Index = () => {
     return (
         <div>
-            <FormLayout heading={heading} message="You are logged in.">
+            <FormLayout heading="Profile Summary" message="">
                 <>
-                    <p>
+                    <InertiaLink
+                            href="/logout"
+                            method="post"
+                            className="text-blue-500 text-right"
+                        >
+                            Log out
+                        </InertiaLink>
+                    <div className="flex flex-col  p-6 mb-12">
+                        <div className="mx-auto rounded-md p-6 bg-gray shadow border w-full mb-12 flex flex-col ">
+                            <div className=" flex w-1/4 sm:w-1/3 justify-center mx-auto mb-6 text-sm text-gray-900">
+                                <p className="flex w-1/2 justify-start">Name: </p>
+                                <p className="flex w-1/2 justify-start">Test</p>
+                            </div>
+                            <div className="flex w-1/4 sm:w-1/3 justify-center test-center w-1/2 mx-auto mb-6 text-sm text-gray-900">
+                                <p className="flex w-1/2 justify-start">Email:</p>
+                                <p className="flex w-1/2 justify-start">Test@example.com</p>
+                            </div>
+                            <div className="flex w-1/4 sm:w-1/3 justify-center mx-auto mb-2 text-sm text-gray-900">
+                                <p className="flex w-1/2 justify-start">Slack Handle: </p>
+                                <p className="flex w-1/2 justify-start">Test</p>
+                            </div>
+                         </div>
+           
+                        <div className="flex sm:flex-col lg:flex-row mb-12">
+                            <div className="w-1/2 sm:w-full">
+                                <div className="rounded-md p-6 bg-gray shadow border-solid border-2 border-gray-600">
+                                    <div className="mb-2 flex flex-col">
+                                        <h3 className="text-sm text-gray-900 mb-6 py-2 text-center">
+                                            Are you interested in becomeing mentee and learning new skill? If so,please complete your mentor profile</h3>
+                                            <InertiaLink
+                                                href="/profiles/mentee/new"
+                                                className="text-blue-500">
+                                                    <button type="button" className="block mx-auto px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150"> Mentee form</button>
+                                            </InertiaLink>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="w-1/2 sm:w-full lg:ml-4 lg:mt-0 sm:ml-0 sm:mt-12">
+                                <div className="rounded-md p-6 bg-gray shadow border-solid border-2 border-gray-600">
+                                    <div className="mb-2 flex flex-col">
+                                        <h3 className="text-sm text-gray-900 mb-6 py-2 text-center"> 
+                                        Are you interested in mentoring people and sharing your knowledge? If so,please complete your mentor profile</h3>
+                                        <InertiaLink href="/profiles/mentor/new" className="text-blue-500">
+                                            <button type="button" className="block mx-auto px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150">
+                                                Mentor form
+                                            </button>
+                                        </InertiaLink> 
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+           
+                    </div>
+                </>
+            </FormLayout>
+        </div>
+    );
+};        
+                   {/* <p>
                         Here you can create a profile with general information
                         about yourself.
                         <InertiaLink
@@ -57,11 +114,7 @@ const Index = ({ heading }: Props) => {
                     >
                         Log out
                     </InertiaLink>
-                    <p>&nbsp;</p>
-                </>
-            </FormLayout>
-        </div>
-    );
-};
-
+                   <p>&nbsp;</p>*/}
+ 
+            
 export default Index;
