ff41f8a143c7ec0e9c49a85f3db312ad84310c76
John Wyles <john@fourstacks.co.uk>
2020-07-17 20:51:47 +0100

    Update card component to accept any react node as a header/footer

--- resources/js/Molecules/Card.tsx
+++ resources/js/Molecules/Card.tsx
@@ -1,33 +1,28 @@
 import React from "react";
 
 interface IProps {
-    heading?: String;
-    hasPadding?: Boolean;
+    header?: React.ReactNode;
+    footer?: React.ReactNode;
 }
 
-const Card: React.FC<IProps> = ({ heading, hasPadding, children}) => {
+const Card: React.FC<IProps> = ({ header, footer, children}) => {
+    
     return (
-        <section>
-            <div className="bg-white shadow overflow-hidden sm:rounded-lg">
-                <div
-                    className={
-                        hasPadding
-                            ? "px-4 py-5 border-b border-gray-200 sm:px-6"
-                            : "border-b border-gray-200 sm:px-6"
-                    }
-                >
-                    <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                        {heading ?? (
-                            <h3 className="text-lg leading-6 font-medium text-gray-900">
-                                {heading}
-                            </h3>
-                        )}                                           
-                    </div>
-
-                    {children}
+        <div className="bg-white overflow-hidden shadow rounded-lg">
+            { header && (
+                <div className="border-b border-gray-200 px-4 py-5 sm:px-6">
+                    {header}
                 </div>
+            )}
+            <div className="px-4 py-5 sm:p-6">
+                {children}
             </div>
-        </section>
+            { footer && (
+                <div className="border-t border-gray-200 px-4 py-4 sm:px-6">
+                    {footer}
+                </div>
+            )}
+        </div>
     );
 };
 
