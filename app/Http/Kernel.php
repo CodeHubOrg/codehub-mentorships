6a9467f99494831e1f4339ae0f6f82bc3cb942b7
Katja Durrani <mail@kdurrani..co.uk>
2020-08-22 01:58:30 +0100

    remove unused middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,6 +63,5 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'convert.keys' => \App\Http\Middleware\ConvertInputKeys::class,
     ];
 }
