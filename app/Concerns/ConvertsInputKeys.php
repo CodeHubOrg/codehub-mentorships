8acd52cd5cba621941dc56eea6c6f7dff463bfe4
Katja Durrani <mail@kdurrani..co.uk>
2020-08-24 00:31:42 +0100

    add missing new files!

---
+++ app/Concerns/ConvertsInputKeys.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Concerns;
+
+use Illuminate\Support\Str;
+
+trait ConvertsInputKeys {
+	
+	public function prepareForValidation() {
+
+		$convertedKeys = collect($this->input())
+            ->mapWithKeys(function ($value, $key) {
+                return [Str::snake($key) => $value];
+            })
+            ->all();
+
+        $this->merge($convertedKeys);
+
+	}
+}
