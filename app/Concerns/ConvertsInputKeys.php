3dc4e636cb5039b1e7dd4b64079f368ee738d7b8
Katja Durrani <mail@kdurrani..co.uk>
2020-08-24 00:31:42 +0100

    add missing new files!

--- app/Concerns/ConvertsInputKeys.php
+++ app/Concerns/ConvertsInputKeys.php
@@ -4,11 +4,12 @@ namespace App\Concerns;
 
 use Illuminate\Support\Str;
 
-trait ConvertsInputKeys
-{
-    public function prepareForValidation()
-    {
-        $convertedKeys = collect($this->input())
+
+trait ConvertsInputKeys {
+	
+	public function prepareForValidation() {
+
+		$convertedKeys = collect($this->input())
             ->mapWithKeys(function ($value, $key) {
                 return [Str::snake($key) => $value];
             })
