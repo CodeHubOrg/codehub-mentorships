78d7f689aa9412950b67456e36d58787a2d8c6b9
John Wyles <john@fourstacks.co.uk>
2020-08-23 12:13:49 +0100

    Apply fixes from StyleCI (#92)

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -1,11 +1,11 @@
 <?php
 
 namespace App\Http\Controllers\Dashboard;
+
 use App\Models\MenteeProfile;
 use App\Models\MentorProfile;
 use App\Models\MentorProfileMenteeProfile;
 use App\Models\User;
-
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
@@ -25,29 +25,29 @@ class DashboardController
         $mentor = $u->mentorProfile ? $h->addCamelsToModel($u->mentorProfile) : null;
         $mentee = $u->menteeProfile ? $h->addCamelsToModel($u->menteeProfile) : null;
 
-        $pairedMentor = $mentee ? MentorProfileMenteeProfile::where('mentee_profile_id', $mentee->id)->value('mentor_profile_id'):null;
+        $pairedMentor = $mentee ? MentorProfileMenteeProfile::where('mentee_profile_id', $mentee->id)->value('mentor_profile_id') : null;
         $pairedMentorUserID = MentorProfile::where('id', $pairedMentor)->value('user_id');
-        $pairedMentor=User::where('id', $pairedMentorUserID)->get();
-  
-        $pairedMentees = $mentor ? MentorProfileMenteeProfile::where('mentor_profile_id', $mentor->id)->pluck('mentee_profile_id'):null;
-       if($pairedMentees){
-        foreach ($pairedMentees as $pairedMentee) {
-            $pairedMenteeUserID = MenteeProfile::where('id', $pairedMentee)->value('user_id');
-            $pairedMentee=User::find($pairedMenteeUserID);
-             
-            $attr['name'] = $pairedMentee->name;
-            $attr['email'] = $pairedMentee->email;
-            $attr['slackHandle'] = $pairedMentee->slack_handle;
-            $pairedMenteelist[] = $attr;
+        $pairedMentor = User::where('id', $pairedMentorUserID)->get();
+
+        $pairedMentees = $mentor ? MentorProfileMenteeProfile::where('mentor_profile_id', $mentor->id)->pluck('mentee_profile_id') : null;
+        if ($pairedMentees) {
+            foreach ($pairedMentees as $pairedMentee) {
+                $pairedMenteeUserID = MenteeProfile::where('id', $pairedMentee)->value('user_id');
+                $pairedMentee = User::find($pairedMenteeUserID);
+
+                $attr['name'] = $pairedMentee->name;
+                $attr['email'] = $pairedMentee->email;
+                $attr['slackHandle'] = $pairedMentee->slack_handle;
+                $pairedMenteelist[] = $attr;
+            }
         }
-       } 
-        
+
         return Inertia::render('Dashboard/Show', [
             'user' => $u,
             'mentorProfile' => $mentor,
             'menteeProfile' => $mentee,
             'pairedMentor' => $pairedMentor,
-            'pairedMentees' => $pairedMenteelist
+            'pairedMentees' => $pairedMenteelist,
         ]);
     }
 }
