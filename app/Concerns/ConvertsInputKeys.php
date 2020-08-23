1f971e855b93ef42f9d19c397f3723df6c6bdcd0
John Wyles <john@fourstacks.co.uk>
2020-08-24 00:32:13 +0100

    Apply fixes from StyleCI (#93)

--- app/Concerns/ConvertsInputKeys.php
+++ app/Concerns/ConvertsInputKeys.php
@@ -4,17 +4,16 @@ namespace App\Concerns;
 
 use Illuminate\Support\Str;
 
-trait ConvertsInputKeys {
-	
-	public function prepareForValidation() {
-
-		$convertedKeys = collect($this->input())
+trait ConvertsInputKeys
+{
+    public function prepareForValidation()
+    {
+        $convertedKeys = collect($this->input())
             ->mapWithKeys(function ($value, $key) {
                 return [Str::snake($key) => $value];
             })
             ->all();
 
         $this->merge($convertedKeys);
-
-	}
+    }
 }
