17537266df83b60fa286e351ad16022706f4bc58
John Wyles <john@fourstacks.co.uk>
2020-09-27 08:43:01 +0100

    Add new auth layout

--- resources/js/Layouts/AuthLayout.tsx
+++ resources/js/Layouts/AuthLayout.tsx
@@ -1,29 +1,30 @@
 import React from "react";
+import AuthBackground from '@/Molecules/AuthBackground';
 
-interface Props {
-    children: React.ReactNode;
-    heading: string;
-    message: string;
+interface Props{
+    children: React.ReactNode,
+    title: string
 }
 
-export default function AuthLayout({ children, heading, message }: Props) {
+export default function AuthLayout({ children, title } : Props) {
     return (
-        <>
-            <div className=" bg-gray-50 flex flex-col sm:px-6">
-                <div className="sm:mx-auto sm:w-full sm:max-w-md">
-                    <h2 className="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900 p-4">
-                        {heading}
-                    </h2>
-                    <p className="mt-2 text-center text-sm leading-5 text-gray-600 max-w">
-                        {message}
-                    </p>
-                </div>
-                <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-                    <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-                        {children}
-                    </div>
-                </div>
+        <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+            <AuthBackground />
+
+            <div className="sm:mx-auto sm:w-full sm:max-w-md">
+                <img
+                    className="block mx-auto h-8 w-auto"
+                    src="/img/codehub.svg"
+                    alt="Code Hub logo"
+                />
+                <h2 className="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900">
+                    { title }
+                </h2>
             </div>
-        </>
+
+            <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+                { children }
+            </div>
+        </div>
     );
 }
