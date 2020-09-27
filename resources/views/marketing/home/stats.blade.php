c8b20a3328ece6a61bd4aafbdac9a01a0860165c
John Wyles <john@fourstacks.co.uk>
2020-09-27 08:42:36 +0100

    Add homepage layout

---
+++ resources/views/marketing/home/stats.blade.php
@@ -0,0 +1,38 @@
+<div class="bg-indigo-800">
+    <div class="max-w-screen-xl mx-auto py-12 px-4 sm:py-16 sm:px-6 lg:px-8 lg:py-20">
+        <div class="max-w-4xl mx-auto text-center">
+            <h2 class="text-3xl leading-9 font-extrabold text-white sm:text-4xl sm:leading-10">
+                Join our community
+            </h2>
+            <p class="mt-3 text-xl leading-7 text-indigo-200 sm:mt-4">
+                From beginners to seasoned professionals, the CodeHub mentorship scheme offers a fantastic opportunity to learn and to teach.
+            </p>
+        </div>
+        <dl class="mt-10 text-center sm:max-w-3xl sm:mx-auto sm:grid sm:grid-cols-3 sm:gap-8">
+            <div class="flex flex-col">
+                <dt class="order-2 mt-2 text-lg leading-6 font-medium text-indigo-200">
+                    Mentors
+                </dt>
+                <dd class="order-1 text-5xl leading-none font-extrabold text-white">
+                    12
+                </dd>
+            </div>
+            <div class="flex flex-col mt-10 sm:mt-0">
+                <dt class="order-2 mt-2 text-lg leading-6 font-medium text-indigo-200">
+                    Mentees
+                </dt>
+                <dd class="order-1 text-5xl leading-none font-extrabold text-white">
+                    32
+                </dd>
+            </div>
+            <div class="flex flex-col mt-10 sm:mt-0">
+                <dt class="order-2 mt-2 text-lg leading-6 font-medium text-indigo-200">
+                    Technologies covered
+                </dt>
+                <dd class="order-1 text-5xl leading-none font-extrabold text-white">
+                    7
+                </dd>
+            </div>
+        </dl>
+    </div>
+</div>
