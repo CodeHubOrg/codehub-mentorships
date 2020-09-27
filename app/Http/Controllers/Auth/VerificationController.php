5c0f791fd5f0fa2a9afe787eda564879fc6f3e6e
John Wyles <john@fourstacks.co.uk>
2020-09-27 08:41:18 +0100

    Fix key name for better consistency

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -100,11 +100,11 @@ class VerificationController extends Controller
         }
 
         return $request->wantsJson()
-                    ? new Response('', 204)
-                    : Inertia::render('Dashboard/Show', [
-                        'user' => UserPresenter::make($user),
-                        'justverified' => true,
-                    ]);
+            ? new Response('', 204)
+            : Inertia::render('Dashboard/Show', [
+                'user' => UserPresenter::make($user),
+                'justVerified' => true,
+            ]);
     }
 
     public function resend(Request $request)
