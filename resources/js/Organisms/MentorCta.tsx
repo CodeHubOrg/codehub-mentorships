37b1461ca9c99a5b7f762a7ff063c8416b37ad32
John Wyles <john@fourstacks.co.uk>
2020-09-27 08:43:58 +0100

    Add new layout for dashboard - new state

---
+++ resources/js/Organisms/MentorCta.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { InertiaLink } from '@inertiajs/inertia-react';
+
+export const MentorCta: React.FC = () => {
+    return (
+        <div className="flex flex-col items-center sm:flex-row lg:flex-col">
+            <img
+                className="max-w-xs sm:mr-8 sm:w-1/2 lg:max-w-xs lg:mr-0 lg:mb-6"
+                src="/img/mentor.svg"
+                alt="Mentee cartoon illustration"
+            />
+            <div className="flex flex-col justify-center">
+                <h3 className="text-center py-6 sm:pt-0 sm:text-left lg:pt-6 lg:text-center">
+                    Are you interested in mentoring people
+                    and sharing your knowledge? If so,please
+                    complete your mentor profile
+                </h3>
+                <InertiaLink
+                    href="/profiles/mentee/new"
+                    className="inline-flex self-center items-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150 sm:self-start lg:self-center"
+                >
+                    Apply to become a mentor
+                </InertiaLink>
+            </div>
+        </div>
+    );
+};
--- resources/js/Pages/Dashboard/Show.tsx
+++ resources/js/Pages/Dashboard/Show.tsx
@@ -1,135 +1,78 @@
-import React from "react";
-import { InertiaLink } from "@inertiajs/inertia-react";
+import React, { useState } from "react";
 import ProfileMenteeSection from "@/Organisms/ProfileMenteeSection";
 import ProfileMentorSection from "@/Organisms/ProfileMentorSection";
 import { AppLayout } from "@/Layouts/AppLayout";
+import { SuccessBanner } from '@/Molecules/Banners/SuccessBanner';
+import { User } from '@/Interfaces/User';
+import { MentorProfile } from '@/Interfaces/MentorProfile';
+import { MenteeProfile } from '@/Interfaces/MenteeProfile';
+import { MenteeCta } from '@/Organisms/MenteeCta';
+import { MentorCta } from '@/Organisms/MentorCta';
 
 interface IProps {
     heading?: string;
-    justverified?: boolean;
-    user: {
-        name: string;
-        email: string;
-        slackHandle?: string;
-    };
-    mentorProfile?: {
-        id: number;
-        mentoringExperience: string;
-        interests?: string;
-        skillset?: string;
-        suitableTime?: string;
-        extraInfo?: string;
-        status?: number;
-    };
-    menteeProfile?: {
-        id: number;
-        currentStatus: string;
-        previousExperience: string;
-        interests: string;
-        specificInterests: string;
-        mentoringType?: string;
-        timeframe?: string;
-        suitableTime?: string;
-        extraInfo?: string;
-        status?: number;
-    };
+    justVerified?: boolean;
+    user: User;
+    mentorProfile?: MentorProfile;
+    menteeProfile?: MenteeProfile;
 }
 
-const Show = ({ user, mentorProfile, menteeProfile, justverified }: IProps) => {
-    const { name, email, slackHandle } = user;
+const Show = ({ user, mentorProfile, menteeProfile, justVerified }: IProps) => {
+    // const { name, email, slackHandle } = user;
+    const [verificationBannerVisible, setVerificationBannerVisible] = useState(justVerified)
 
     return (
-        <AppLayout heading="Welcome to CodeHub Mentorships">
-            {justverified && (
-                <div className="flex flex-col">
-                    <div className="mx-auto text-center rounded-md p-6 bg-gray shadow border w-full mb-12 flex flex-col bg-gray-100">
-                        <p>Thank you, your email has just been verified.</p>
-                    </div>
-                </div>
+        <AppLayout>
+            {verificationBannerVisible && (
+                <SuccessBanner onDismiss={setVerificationBannerVisible}>
+                    <p>Thank you, your email has been verified.</p>
+                </SuccessBanner>
             )}
-            <div className="flex flex-col">
-                <div className="mx-auto rounded-md p-6 bg-gray shadow border w-full mb-12 flex flex-col bg-gray-100">
-                    <div className=" flex w-full md:w-1/3 justify-center mx-auto mb-6 text-sm text-gray-900">
-                        <p className="flex w-1/2 justify-start">Name: </p>
-                        <p className="flex w-1/2 justify-start">{name}</p>
-                    </div>
-                    <div className="flex w-full md:w-1/3 justify-center test-center w-1/2 mx-auto mb-6 text-sm text-gray-900">
-                        <p className="flex w-1/2 justify-start">Email:</p>
-                        <p className="flex w-1/2 justify-start">{email}</p>
-                    </div>
-                    <div className="flex w-full md:w-1/3 justify-center mx-auto mb-2 text-sm text-gray-900">
-                        <p className="flex w-1/2 justify-start">
-                            Slack Handle:
-                        </p>
-                        <p className="flex w-1/2 justify-start">
-                            {slackHandle}
-                        </p>
-                    </div>
-                </div>
 
-                <div className="flex flex-col lg:flex-row mb-12 lg:mb-24">
-                    <div className="lg:w-1/2 w-full bg-gray-100">
-                        <div className="rounded-md p-6 bg-gray shadow border">
-                            {menteeProfile ? (
-                                <div>
-                                    <ProfileMenteeSection
-                                        heading="Mentee profile"
-                                        menteeProfile={menteeProfile}
-                                    />
-                                </div>
-                            ) : (
-                                <div className="mb-2 flex flex-col">
-                                    <h3 className="text-sm text-gray-900 mb-6 py-2 text-center">
-                                        Are you interested in becomeing a mentee
-                                        and learn new skills? If so, please
-                                        complete your mentee profile
-                                    </h3>
-                                    <InertiaLink
-                                        href="/profiles/mentee/new"
-                                        className="text-blue-500"
-                                    >
-                                        <button
-                                            type="button"
-                                            className="block mx-auto px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150"
-                                        >
-                                            {" "}
-                                            Mentee form
-                                        </button>
-                                    </InertiaLink>
-                                </div>
-                            )}
-                        </div>
+            <h1 className="text-3xl font-semibold leading-tight text-gray-900 mb-8">
+                Hello {user.firstName}!  Welcome to CodeHub Mentorships
+            </h1>
+
+            <div className="flex flex-col">
+                {/*<div className="mx-auto rounded-md p-6 bg-gray shadow border w-full mb-12 flex flex-col bg-gray-100">*/}
+                {/*    <div className=" flex w-full md:w-1/3 justify-center mx-auto mb-6 text-sm text-gray-900">*/}
+                {/*        <p className="flex w-1/2 justify-start">Name: </p>*/}
+                {/*        <p className="flex w-1/2 justify-start">{name}</p>*/}
+                {/*    </div>*/}
+                {/*    <div className="flex w-full md:w-1/3 justify-center test-center w-1/2 mx-auto mb-6 text-sm text-gray-900">*/}
+                {/*        <p className="flex w-1/2 justify-start">Email:</p>*/}
+                {/*        <p className="flex w-1/2 justify-start">{email}</p>*/}
+                {/*    </div>*/}
+                {/*    <div className="flex w-full md:w-1/3 justify-center mx-auto mb-2 text-sm text-gray-900">*/}
+                {/*        <p className="flex w-1/2 justify-start">*/}
+                {/*            Slack Handle:*/}
+                {/*        </p>*/}
+                {/*        <p className="flex w-1/2 justify-start">*/}
+                {/*            {slackHandle}*/}
+                {/*        </p>*/}
+                {/*    </div>*/}
+                {/*</div>*/}
+
+                <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 mb-12 lg:mb-24">
+                    <div className="w-full bg-white p-6 rounded shadow">
+                        {menteeProfile ? (
+                            <ProfileMenteeSection
+                                heading="Mentee profile"
+                                menteeProfile={menteeProfile}
+                            />
+                        ) : (
+                            <MenteeCta></MenteeCta>
+                        )}
                     </div>
-                    <div className="lg:w-1/2 w-full lg:ml-4 lg:mt-0 ml-0 mt-12 bg-gray-100">
-                        <div className="rounded-md p-6 bg-gray shadow border">
-                            {mentorProfile ? (
-                                <div>
-                                    <ProfileMentorSection
-                                        heading="Mentor profile"
-                                        mentorProfile={mentorProfile}
-                                    />
-                                </div>
-                            ) : (
-                                <div className="mb-2 flex flex-col">
-                                    <h3 className="text-sm text-gray-900 mb-6 py-2 text-center">
-                                        Are you interested in mentoring people
-                                        and sharing your knowledge? If so,please
-                                        complete your mentor profile
-                                    </h3>
-                                    <InertiaLink
-                                        href="/profiles/mentor/new"
-                                        className="text-blue-500"
-                                    >
-                                        <button
-                                            type="button"
-                                            className="block mx-auto px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150"
-                                        >
-                                            Mentor form
-                                        </button>
-                                    </InertiaLink>
-                                </div>
-                            )}
-                        </div>
+                    <div className="w-full bg-white p-6 rounded shadow">
+                        {mentorProfile ? (
+                            <ProfileMentorSection
+                                heading="Mentor profile"
+                                mentorProfile={mentorProfile}
+                            />
+                        ) : (
+                            <MentorCta></MentorCta>
+                        )}
                     </div>
                 </div>
             </div>
