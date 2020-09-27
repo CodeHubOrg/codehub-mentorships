ff8e107b0e08f7ff8edad1643da49271426d0a88
John Wyles <john@fourstacks.co.uk>
2020-09-27 08:43:17 +0100

    Apply new auth layout to login and register

--- resources/js/Pages/Auth/Register/Create.tsx
+++ resources/js/Pages/Auth/Register/Create.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Form, { IErrors } from "@/Organisms/Form";
 import FormTextInput from "@/Molecules/FormTextInput";
-import { AppLayout } from "@/Layouts/AppLayout";
 import AuthLayout from "@/Layouts/AuthLayout";
+import { InertiaLink } from '@inertiajs/inertia-react';
 
 type RegisterFormValues = {
     firstName: string;
@@ -37,111 +37,111 @@ const validate = (values: RegisterFormValues) => {
 
 const Create: React.FC = () => {
     return (
-        <div>
-            <AppLayout admin="false">
-                <AuthLayout
-                    heading="Register"
-                    message="Please register to create a profile"
-                >
-                    <Form<RegisterFormValues>
-                        action="/register"
-                        initialValues={{
-                            firstName: "",
-                            lastName: "",
-                            email: "",
-                            slackHandle: "",
-                            password: "",
-                            password_confirmation: "",
-                        }}
-                        validate={validate}
-                        button="Register"
-                        render={(
-                            values,
-                            handleChange,
-                            errors,
-                            errorsFromBackend,
-                        ) => (
-                            <React.Fragment>
-                                <FormTextInput
-                                    type="text"
-                                    name="firstName"
-                                    label="First Name"
-                                    value={values.firstName}
-                                    helpText=""
-                                    onChange={handleChange}
-                                />
+        <AuthLayout title="Create an account">
+            <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+            <Form<RegisterFormValues>
+                action="/register"
+                initialValues={{
+                    firstName: "",
+                    lastName: "",
+                    email: "",
+                    slackHandle: "",
+                    password: "",
+                    password_confirmation: "",
+                }}
+                validate={validate}
+                button="Register"
+                render={(
+                    values,
+                    handleChange,
+                    errors,
+                    errorsFromBackend,
+                ) => (
+                    <React.Fragment>
+                        <FormTextInput
+                            type="text"
+                            name="firstName"
+                            label="First Name"
+                            value={values.firstName}
+                            helpText=""
+                            onChange={handleChange}
+                        />
 
-                                <div className="block text-sm font-medium text-red-500 pb-5">
-                                    {errors.firstName ||
-                                        errorsFromBackend.firstName}
-                                </div>
-                                <FormTextInput
-                                    type="text"
-                                    name="lastName"
-                                    label="Last Name"
-                                    value={values.lastName}
-                                    helpText=""
-                                    onChange={handleChange}
-                                />
-                                <div className="block text-sm font-medium text-red-500 pb-5">
-                                    {errors.lastName ||
-                                        errorsFromBackend.lastName}
-                                </div>
-                                <FormTextInput
-                                    type="email"
-                                    name="email"
-                                    label="Email address"
-                                    value={values.email}
-                                    helpText=""
-                                    onChange={handleChange}
-                                />
-                                <div className="block text-sm font-medium text-red-500 pb-5">
-                                    {errors.email || errorsFromBackend.email}
-                                </div>
-                                <FormTextInput
-                                    type="text"
-                                    name="slackHandle"
-                                    label="Slack handle"
-                                    value={values.slackHandle}
-                                    helpText=""
-                                    onChange={handleChange}
-                                />
-                                <div className="block text-sm font-medium text-red-500 pb-5" />
-                                <FormTextInput
-                                    type="password"
-                                    name="password"
-                                    label="Password"
-                                    value={values.password}
-                                    helpText=""
-                                    onChange={handleChange}
-                                />
+                        <div className="block text-sm font-medium text-red-500 pb-5">
+                            {errors.firstName ||
+                            errorsFromBackend.firstName}
+                        </div>
+                        <FormTextInput
+                            type="text"
+                            name="lastName"
+                            label="Last Name"
+                            value={values.lastName}
+                            helpText=""
+                            onChange={handleChange}
+                        />
+                        <div className="block text-sm font-medium text-red-500 pb-5">
+                            {errors.lastName ||
+                            errorsFromBackend.lastName}
+                        </div>
+                        <FormTextInput
+                            type="email"
+                            name="email"
+                            label="Email address"
+                            value={values.email}
+                            helpText=""
+                            onChange={handleChange}
+                        />
+                        <div className="block text-sm font-medium text-red-500 pb-5">
+                            {errors.email || errorsFromBackend.email}
+                        </div>
+                        <FormTextInput
+                            type="text"
+                            name="slackHandle"
+                            label="Slack handle"
+                            value={values.slackHandle}
+                            helpText=""
+                            onChange={handleChange}
+                        />
+                        <div className="block text-sm font-medium text-red-500 pb-5" />
+                        <FormTextInput
+                            type="password"
+                            name="password"
+                            label="Password"
+                            value={values.password}
+                            helpText=""
+                            onChange={handleChange}
+                        />
 
-                                <div className="block text-sm font-medium text-red-500 pb-5">
-                                    {errors.password ||
-                                        errorsFromBackend.password}
-                                </div>
-                                <FormTextInput
-                                    type="password"
-                                    name="password_confirmation"
-                                    label="Confirm Password"
-                                    value={values.password_confirmation}
-                                    helpText=""
-                                    onChange={handleChange}
-                                />
+                        <div className="block text-sm font-medium text-red-500 pb-5">
+                            {errors.password ||
+                            errorsFromBackend.password}
+                        </div>
+                        <FormTextInput
+                            type="password"
+                            name="password_confirmation"
+                            label="Confirm Password"
+                            value={values.password_confirmation}
+                            helpText=""
+                            onChange={handleChange}
+                        />
 
-                                <div className="block text-sm font-medium text-red-500 pb-5">
-                                    {errors.password_confirmation ||
-                                        errorsFromBackend.password_confirmation}
-                                </div>
-                            </React.Fragment>
-                        )}
-                    />
-                    <div className="font-medium text-indigo-600 pt-5 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">
-                        <a href="/login">Login</a>
-                    </div>
-                </AuthLayout>
-            </AppLayout>
-        </div>
+                        <div className="block text-sm font-medium text-red-500 pb-5">
+                            {errors.password_confirmation ||
+                            errorsFromBackend.password_confirmation}
+                        </div>
+                    </React.Fragment>
+                )}
+            />
+            </div>
+
+            <p className="mt-4 text-sm leading-5 text-gray-600 max-w">
+                <InertiaLink
+                    href="/"
+                    className="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">
+                    Back to homepage
+                </InertiaLink>
+            </p>
+        </AuthLayout>
     );
 };
 
