87f9d476c239205a7115459ccbdf4d04671fd864
John Wyles <john@fourstacks.co.uk>
2020-10-04 20:14:14 +0100

    Update SVG attrs

--- resources/js/Molecules/Banners/SuccessBanner.tsx
+++ resources/js/Molecules/Banners/SuccessBanner.tsx
@@ -7,13 +7,13 @@ interface Props {
 
 export const SuccessBanner = ({children, onDismiss}: Props) => {
     return (
-        <div className="rounded-md bg-green-50 p-4">
+        <div className="rounded-md bg-green-50 p-4 shadow mb-8">
             <div className="flex">
                 <div className="flex-shrink-0">
                     <svg className="h-5 w-5 text-green-400" viewBox="0 0 20 20" fill="currentColor">
-                        <path fill-rule="evenodd"
+                        <path fillRule="evenodd"
                               d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                              clip-rule="evenodd" />
+                              clipRule="evenodd" />
                     </svg>
                 </div>
                 <div className="ml-3">
@@ -29,9 +29,9 @@ export const SuccessBanner = ({children, onDismiss}: Props) => {
                             onClick={() => onDismiss(false)}
                         >
                             <svg className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                                <path fill-rule="evenodd"
+                                <path fillRule="evenodd"
                                       d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                      clip-rule="evenodd" />
+                                      clipRule="evenodd" />
                             </svg>
                         </button>
                     </div>
