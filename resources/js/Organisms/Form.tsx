d2af4bc7bbf02edde9a7338dcc2eccb5677e7af7
John Wyles <john@fourstacks.co.uk>
2020-10-04 20:14:37 +0100

    Remove conditional in form component

--- resources/js/Organisms/Form.tsx
+++ resources/js/Organisms/Form.tsx
@@ -139,17 +139,13 @@ const Form = <IValues extends Record<string, any>>({
                     errorsFromBackend,
                 )}
 
-                <div className="form-group">
+                <div className="flex justify-end mt-8 border-t border-gray-200 pt-8">
                     {sending ? (
                         <p>Sending...</p>
                     ) : (
                         <button
                             type="submit"
-                            className={
-                                button !== "Login"
-                                    ? "block mx-auto px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150"
-                                    : "w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
-                            }
+                            className="flex py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
                             disabled={haveErrors(errors)}
                         >
                             {button}
