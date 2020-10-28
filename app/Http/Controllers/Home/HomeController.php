8d6931b3cb8062b6de475e110166092d3138bd14
Katja Durrani <mail@kdurrani..co.uk>
2020-10-28 03:30:08 +0000

    remove accidentally committed conflict arrows and other syntax error

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -9,13 +9,8 @@ class HomeController
 {
     public function show()
     {
-<<<<<<< HEAD
         // on the frontend, using data from the auth user for now.
         // so we don't need to pass the user in
-=======
-    	// on the frontend, using data from the auth user for now. 
-    	// so we don't need to pass the user in
->>>>>>> adding route for only verified users back in, also using the auth user from Inertia service provider, don't need to always pass it in
         return Inertia::render('Home/Index');
     }
 }
--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Permission extends extends \Spatie\Permission\Models\Permission
+class Permission extends \Spatie\Permission\Models\Permission
 {
-    //
+    //   
 }
