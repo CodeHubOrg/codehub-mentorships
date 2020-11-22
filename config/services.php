4fd63fb630e6f3878553037b3e5892d1499748f1
Katja Durrani <mail@kdurrani..co.uk>
2020-11-22 02:10:03 +0000

    add postmark adapter for sending mails

--- config/services.php
+++ config/services.php
@@ -29,5 +29,7 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
-
+    'postmark' => [
+        'secret' => env('POSTMARK_SECRET'),
+    ]
 ];
