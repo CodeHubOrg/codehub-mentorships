bed0e647bb8edf0b863fc2e1d6e3e7d5c194bb57
Katja Durrani <mail@kdurrani..co.uk>
2020-11-22 03:22:48 +0000

    remove redundant postmark key in config

--- config/services.php
+++ config/services.php
@@ -29,7 +29,4 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
-    'postmark' => [
-        'secret' => env('POSTMARK_SECRET'),
-    ],
 ];
